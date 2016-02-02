ea188ea3f8b04ed1ed78e9baf4203ea68dcb6549
colinxfleming <c3flemin@gmail.com>
2016-02-02 01:22:21 -0500

    stub factory and test for cases

--- test/factories/cases.rb
+++ test/factories/cases.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :case do
-    
+    line 'DC'
+    initial_call_date { 2.months.ago }   
   end
-
 end
--- test/models/case_test.rb
+++ test/models/case_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CaseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  it 'should do a thing' do 
+    sample_case = build :case
+    assert_equal 'DC', sample_case.line
+  end
 end
