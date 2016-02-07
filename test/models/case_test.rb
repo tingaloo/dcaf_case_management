d2c2a765bf8f3cf3c86b1f32fd00341bbd751ab9
colinxfleming <c3flemin@gmail.com>
2016-02-06 19:15:01 -0500

    stow test for models

--- test/models/case_test.rb
+++ test/models/case_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class CaseTest < ActiveSupport::TestCase
+  describe ''
   it 'should do a thing' do 
     sample_case = build :case
     assert_equal 'DC', sample_case.line
